94d2fddab1da1635ab0d0cd3c01c9f669a0bf482
cley9 <cleyutp@gmail.com>
2023-06-02 02:30:14 -0500

    calculadora

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import ho from './components/ho.vue'
+import calculador from './components/calculadora.vue'
+</script>
+
+<template>
+
+  <HelloWorld msg="Vite + Vue" />
+
+  <!-- <div id="app">
+    <HelloWorld />
+  </div> -->
+<ho/> 
+<calculador/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/calculadora.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>num 1  </h3>
+  <input type="text" v-model="num1"> *
+  <input type="text" v-model="num2">
+  <button @click="sumar"> resultado</button>
+  <p>{{ resul }}</p>
+</template>
+
+<script>
+// console.log();
+export default {
+    data(){
+        return{
+            num1:'',
+            num2:'',
+            resul:0
+        }
+    },
+    methods:{
+        sumar(){
+            const numero1=parseFloat(this.num1);
+            const numero2=parseFloat(this.num2);
+            // const resultado=
+            
+            if (!isNaN(numero1) &&  !isNaN(numero2)) {
+                this.resul=numero1 * numero2;
+            } else {
+                console.log("vacio");
+                
+            }
+            // this.resul=this.num1 + this.num2;
+        }
+    }
+}
+
+
+</script>
